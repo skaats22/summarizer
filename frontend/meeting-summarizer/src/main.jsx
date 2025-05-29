81deb125b766170ede1277c60c6315e7bc70f822
Steve Kaats <stevenakaats@gmail.com>
2025-05-29 13:04:19 -0700

    updated structure

--- frontend/meeting-summarizer/src/main.jsx
+++ frontend/meeting-summarizer/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router";
+import "./index.css";
+import App from "./pages/App/App";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
+);
